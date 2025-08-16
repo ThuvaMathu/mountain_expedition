3ba6a178d99bdca0078190a0dd0d0aab486eecfe
ThuvaMathu <thuvamathu618@gmail.com>
2025-08-16 17:36:58 +1000

    optimised videos

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -63,9 +63,15 @@ export function HeroSection() {
           loop
           muted
           playsInline
+          preload="auto" // ensures video is cached immediately
+          poster="/images/hero-poster.jpg"
         >
           <source
-            src="https://s3.ap-southeast-2.amazonaws.com/images.thuvarakan.info/13831626_1920_1080_60fps.mp4"
+            src="https://s3.ap-southeast-2.amazonaws.com/images.thuvarakan.info/hero-video-hd-1200.webm"
+            type="video/webm"
+          />
+          <source
+            src="https://s3.ap-southeast-2.amazonaws.com/images.thuvarakan.info/hero-video-hd-1200.mp4"
             type="video/mp4"
           />
           Your browser does not support the video tag.
