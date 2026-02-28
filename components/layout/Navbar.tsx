c6265f62d87cf45094258610887c1f2a62b7e6b4
ThuvaMathu <thuvamathu618@gmail.com>
2026-03-01 02:00:28 +1000

    Please Do not make payment

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -33,7 +33,11 @@ export function Navbar() {
 
   return (
     <nav className="bg-white shadow-lg sticky top-0 z-50">
+      <div className=" h-5 py-2 justify-center items-center flex text-red-600 bg-red-100 w-full  " >
+        Please Do not make any payments
+      </div>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+
         <div className="flex justify-between h-20">
           <div className="flex items-center">
             <Link href="/" className="flex items-center space-x-2">
